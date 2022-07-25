d987d3daf76aac38b6d48fcd0609783f2173b6d2
er-ebrahimi <erfan.es1381@gmail.com>
2022-07-26 00:33:22 +0430

    nav scrollup done , scroll section active link done, change background header done, remove menu mobile done, menu show done, show menu done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,43 @@
 /*=============== SHOW MENU ===============*/
-
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close');
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
+if (navMenu) {
+    navToggle.addEventListener('click', () => { navMenu.classList.add('show-menu') })
+}
 
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+navLink.forEach(n =>
+    n.addEventListener('click', () => {
+        const navMenu = document.getElementById('nav-menu');
+        navMenu.classList.remove('show-menu')
+    })
+)
 
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
-
+window.addEventListener('scroll', () => {
+    const header = document.getElementById('header');
+    if (this.scrollY >= 50) {
+        header.classList.add('scroll-header')
+    }
+    else
+        header.classList.remove('scroll-header')
+})
 /*=============== NEW SWIPER ===============*/
 let newSwiper = new Swiper(".product-swiper", {
     spaceBetween: 24,
@@ -34,7 +57,24 @@ let newSwiper = new Swiper(".product-swiper", {
 });
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
 
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SHOW SCROLL UP ===============*/
 
